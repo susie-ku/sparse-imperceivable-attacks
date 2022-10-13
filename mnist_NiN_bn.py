27c6227a426b2c45be9e83903013905ea95d57f4
Ksenia Kuvshinova <48868713+susie-ku@users.noreply.github.com>
2022-10-13 18:17:35 +0300

    Update mnist_NiN_bn.py

--- mnist_NiN_bn.py
+++ mnist_NiN_bn.py
@@ -4,7 +4,7 @@ import scipy.io
 import numpy as np
 
 def conv(x, phase, shape):
-    he_initializer = tf.contrib.keras.initializers.he_normal()
+    he_initializer = tf.compat.v1.contrib.keras.initializers.he_normal()
     W = tf.get_variable('weights', shape=shape, initializer=he_initializer)
     b = tf.get_variable('bias', shape=[shape[3]], initializer=tf.zeros_initializer)
     x = tf.nn.conv2d(x, W, strides=[1,1,1,1], padding='SAME')
@@ -22,49 +22,49 @@ def global_avg_pool(input, k_size=1, stride=1):
     return tf.nn.avg_pool(input, ksize=[1,k_size,k_size,1], strides=[1,stride,stride,1], padding='VALID')
 
 def inference(x, phase=False, keep_prob=1.0):
-    with tf.variable_scope('conv1'):
+    with tf.compat.v1.variable_scope('conv1'):
         x = conv(x, phase, [5, 5, 1, 192])
         x = activation(x)
 
-    with tf.variable_scope('mlp1-1'):
+    with tf.compat.v1.variable_scope('mlp1-1'):
         x = conv(x, phase, [1, 1, 192, 160])
         x = activation(x)
 
-    with tf.variable_scope('mlp1-2'):
+    with tf.compat.v1.variable_scope('mlp1-2'):
         x = conv(x, phase, [1, 1, 160, 96])
         x = activation(x)
 
-    with tf.name_scope('max_pool-1'):
+    with tf.compat.v1.name_scope('max_pool-1'):
         x  = max_pool(x, 3, 2)
 
-    with tf.variable_scope('conv2'):
+    with tf.compat.v1.variable_scope('conv2'):
         x = conv(x, phase, [5, 5, 96, 192])
         x = activation(x)
 
-    with tf.variable_scope('mlp2-1'):
+    with tf.compat.v1.variable_scope('mlp2-1'):
         x = conv(x, phase, [1, 1, 192, 192])
         x = activation(x)
 
-    with tf.variable_scope('mlp2-2'):
+    with tf.compat.v1.variable_scope('mlp2-2'):
         x = conv(x, phase, [1, 1, 192, 192])
         x = activation(x)
 
-    with tf.name_scope('max_pool-2'):
+    with tf.compat.v1.name_scope('max_pool-2'):
         x  = max_pool(x, 3, 2)
 
-    with tf.variable_scope('conv3'):
+    with tf.compat.v1.variable_scope('conv3'):
         x = conv(x, phase, [3, 3, 192, 192])
         x = activation(x)
 
-    with tf.variable_scope('mlp3-1'):
+    with tf.compat.v1.variable_scope('mlp3-1'):
         x = conv(x, phase, [1, 1, 192, 192])
         x = activation(x)
 
-    with tf.variable_scope('mlp3-2'):
+    with tf.compat.v1.variable_scope('mlp3-2'):
         x = conv(x, phase, [1, 1, 192, 10])
         x = activation(x)
 
-    with tf.name_scope('global_avg_pool'):
+    with tf.compat.v1.name_scope('global_avg_pool'):
         x  = global_avg_pool(x, 7, 1)
         output  = tf.reshape(x,[-1,10])
 
